62b9d7ee59044d5339148d0c21472086d462a5e9
Sushmita Anil Maurya <mauryasushmita9422@gmail.com>
2026-07-22 18:25:46 +0530

    Add backend server

---
+++ server.ts
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import connectDB from "./config/db.js"; 
+import authRouter from "./routes/authRoutes.js";
+
+const app = express();
+
+//connect to MongoDB
+await connectDB();
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+app.use("/api/auth", authRouter);
+
+//Global error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error("Unhandled error:", err);
+    res.status(500).json({ message:err.message || "Internal Server Error" 
+        stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+    });
+});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
